07c876b89839cb5001c20d4838d6a095e57c4a64
Daniel Bowring <danieltbowring+github@gmail.com>
2017-09-12 10:37:34 +0900

    Add bomb timer force explode and show timer options

--- bomb-timer/script.js
+++ bomb-timer/script.js
@@ -12,11 +12,13 @@
   };
 
   var ControlManager = function() {
-    var display = null, ready = false, nextTimer = null, timeout = null;
+    var display = null, ready = false, nextTimer = null, timeout = null,
+        displayTimer = false;
     var finishAt = null;
     var elements = {
-      countdown: $('#countdown'),
-      totalTime: $('#totalTime')
+      countdown: $('#setup #countdown'),
+      totalTime: $('#totalTime'),
+      optDisplayTimer: $('#displayTimer')
     }
     
     var init = function() {
@@ -56,7 +58,7 @@
       finishAt = new Date((new Date()).getTime() + (seconds * 1000));
       elements.totalTime.text(seconds.toFixed(3));
       showTimer();
-      display.postMessage({cmd: 'beep'}, '*');
+      display.postMessage({cmd: 'beep', finishAt: displayTimer ? finishAt.getTime() : null}, '*');
     };
 
     var showTimer = function() {
@@ -80,6 +82,16 @@
     var setupEvents = function() {
       $('#start').click(onStart);
       $('#show').click(showDisplay);
+      $('#explode').click(forceExplode);
+      elements.optDisplayTimer.click(updateDisplayTimer);
+    };
+
+    var forceExplode = function() {
+      onTimerEnd()
+    };
+
+    var updateDisplayTimer = function() {
+      displayTimer = !!elements.optDisplayTimer.is(':checked');
     };
 
     var onStart = function() {
@@ -112,7 +124,8 @@
   };
 
   var DisplayManager = function() {
-    var parent = window.opener.window, timer = null, timeout = null;
+    var parent = window.opener.window, timer = null, timeout = null,
+        finishAt = null;
     var audio = {
       beep: document.createElement('audio'),
       explode: document.createElement('audio')
@@ -122,7 +135,9 @@
     audio.beep.preload = 'auto';
     audio.explode.src = 'explode.ogg';
     audio.explode.preload = 'auto';
-    
+    var elements = {
+      timer: $('#main-ui .countdown')
+    };
 
     var init = function() {
       $('#setup').hide();
@@ -142,12 +157,48 @@
         audio.explode.pause();
         audio.explode.currentTime = 0;
         audio.beep.play();
+        finishAt = data.finishAt;
+        updateCountdown()
       } else if (data.cmd === 'explode') {
         $('.flasher').removeClass('active').addClass('exploded');
         audio.beep.pause();
         audio.beep.currentTime = 0;
         audio.explode.play();
+        finishAt = null;
       }
+    };
+
+    var updateCountdown = function() {
+      console.log('updateCountDown', finishAt);
+      if (!finishAt) {
+        elements.timer.text('');
+        return;
+      }
+      var delta = finishAt - (new Date()).getTime();
+      console.log('updateCountDown >>>', delta);
+      if (delta <= 0) {
+        elements.timer.text('');
+        return;
+      }
+
+      elements.timer.text(getClockValue(delta));
+      window.requestAnimationFrame(updateCountdown);
+    };
+
+    var getClockValue = function(delta) {
+      var seconds = Math.floor(delta / 1000)
+      var ms = delta % 1000;
+      var minutes = Math.floor(seconds / 60);
+      seconds -= minutes * 60;
+      return zpad(minutes, 2) + ':' + zpad(seconds, 2) + ':' + zpad(ms, 3);
+    };
+
+    var zpad = function(intVal, spaces) {
+      var s = intVal.toString();
+      while (s.length < spaces) {
+        s = '0' + s;
+      }
+      return s;
     };
 
     return {
